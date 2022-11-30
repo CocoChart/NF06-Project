2196084307ed3894c5ad269c60e7fe59f56684a5
CocoChart <corentinchartier334@gmail.com>
2022-11-30 13:52:53 +0100

    réorganisation et un peu d'avancée sur python

---
+++ code/Code.py
@@ -0,0 +1,25 @@
+import matplotlib as plt
+import numpy as np
+import pandas as pd
+from math import *
+from ctypes import *
+from tkinter import * 
+
+functions = CDLL("./functions.dll")
+
+with open("./Conso.csv") as f :
+    conso = pd.read_csv(f)
+print(conso)
+
+print(conso["X"]["Mar"])
+
+vente = c_int(0)
+stock = c_int (0)
+seuil = c_int(0)
+nombre_de_commande = c_int(0)
+taille_commande =  c_int(0)
+delais = c_int(0)
+prix_stock = c_int(0)
+cout_total = c_int(0)
+
+#functions.calcul(pointer(vente), pointer(stock), pointer(seuil), pointer(nombre_de_commande), pointer(taille_commande), pointer(delais), pointer(prix_stock), pointer(cout_total))
