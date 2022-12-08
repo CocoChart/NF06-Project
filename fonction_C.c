4d9e7dbd09eed170804467074d5abbc4e38ebf9d
Nagala2 <mael.javer_kala@utt.fr>
2022-12-08 14:45:59 +0100

    Vérifier qu'on puisse bien appeller toute les fonctions indépendement

--- fonction_C.c
+++ fonction_C.c
@@ -4,15 +4,17 @@
 
 void reception(int* delais, int *stock, int *commande) // Fonction qui vérifie si une commande est arrivé et ajuste les stock en conséquence
 {
+    printf("\nrecepetion\n");
     if (*delais == 0) // Vérifie si le délais de livraison est nul, cette condition est vérifier quand une commande viens d'arriver ou quand il n'y a pas de commande en cour 
     { 
         *stock += *commande; // incrémente le stock de la valeur de la commande
         *commande = 0; // remet la valeur du stock à 0 pour éviter d'augmenter plusieurs fois le stock avec la même commande
     }
+    
 }
 void passage_commande_compteur_mois(int *stock, int *delais, int *commande, int *nombre_commande, int temp_commande, int seuil, float *cout_total, float prix, float prix_commande)
 { // Fonction qui vérifie les stocks, passe une commande si besoin et actualise le delais de livraison 
-   
+    printf("passage de commande et compteur mois\n");
     if (*stock <= seuil && *delais == 0) // vériie si les stock sont en dessous du seuil et qu'aucune commande n'est déjà en cours. si ces conditions sont vérifié alors on passe une nouvlle commande
     { 
         *delais = temp_commande; // incrémente le delais de livraison du temp de livraison determiné par le produit concerné
@@ -29,6 +31,7 @@ void passage_commande_compteur_mois(int *stock, int *delais, int *commande, int 
 void vente_et_prix_stockage(int vente, int *stock, float *cout_stock, float prix, float prix_stockage, float* cout_total)
 // Fonction qui diminue le stock du montant de la vente, vérifie si il y a rupture de stock et calcul le prix de stockage des produits restants
 {
+    printf("vente et prix stockage\n");
     *stock -= vente; // diminue les stock 
     if (*stock < 0 ) // vérifie si il y a une rupture de stock
     {
@@ -48,6 +51,7 @@ void calcul(int *stock, int seuil, int *nombre_de_commande,int *taille_commande,
     printf("prix_commande : %.2f\n", prix_commande);
     printf("temp_commande : %d\n\n", temp_commande);
     printf("cout total : %.2f\n", *cout_total);
+
     int vente =0 ; 
     int commande = 0;
     int nombre_commande = 0;
@@ -67,8 +71,8 @@ void calcul(int *stock, int seuil, int *nombre_de_commande,int *taille_commande,
         passage_commande_compteur_mois(stock, &delais, &commande, &nombre_commande, temp_commande, seuil, cout_total, prix, prix_commande);
         vente_et_prix_stockage(vente,stock, cout_stock,prix, prix_stockage, cout_total);
     }
-    // on affiche les différentes valeurs de cout et le stock maximal attend pendant l'année
-    reception(&delais,stock,&commande); 
+    reception(&delais,stock,&commande); // on vérifie une dernière fois si une commande est en cours. Cela correspond à une potentiel commande passé en décembre et qui n'arriverais que début janvier
+    // on affiche les différentes valeurs de cout et le stock maximal atteint pendant l'année
     printf("voici la valeur de stock final : %d\n" ,*stock);
     printf("voici la valeur du cout de stockage : %.2f\n", *cout_stock );
     *cout_total += *cout_stock;
